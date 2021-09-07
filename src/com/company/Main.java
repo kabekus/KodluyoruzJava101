307af82a9b0b6f834354ed824a3cf9a1e6b38bf3
Kabe KUŞ <kabekus.123@gmail.com>
2021-09-07 23:12:28 +0300

    Taksimetre Hesaplama

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,17 @@ import  java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int kenarA, kenarB, kenarC, alan, u;
-        Scanner kenar = new Scanner(System.in);
-        System.out.print("A kenarı: ");
-        kenarA = kenar.nextInt();
-        System.out.print("B kenarı: ");
-        kenarB = kenar.nextInt();
-        System.out.print("C kenarı: ");
-        kenarC = kenar.nextInt();
-        u = (kenarA+kenarB+kenarC)/2;
-        alan= (int) Math.sqrt(u*(u-kenarA)*(u-kenarB)*(u-kenarC));
-        System.out.print("Belirlediğiniz Üçgenin Alanı: "+alan);
+        int km;
+        double kmTutar=2.20, toplam, baslangicUcret=10;
+
+        Scanner taksiMetre = new Scanner(System.in);
+        System.out.print("Gidilecek Yolu KM Cinsinden Girin: ");
+        km=taksiMetre.nextByte();
+
+        toplam = km*kmTutar;
+        toplam += baslangicUcret;
+
+        toplam = (toplam<20) ? 20 : toplam;
+        System.out.print("Toplam Ücret: " + toplam);
     }
 }
