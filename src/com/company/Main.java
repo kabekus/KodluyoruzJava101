60ef51cf605042ce01c48fde413b902fb047bdc2
Kabe KUŞ <kabekus.123@gmail.com>
2021-09-09 17:51:43 +0300

    Pratik - Hesap Makinesi (Switch)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,23 +5,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        float armutFiyat=2.14f, elmaFiyat=3.67f, domatesFiyat=1.11f,
-                muzFiyat=0.95f, patlicanFiyat=5, toplamTutar;
-        int armut, elma, domates, muz, patlican;
+        int sayi1, sayi2, secim;
+        float islem;
 
-        Scanner manav = new Scanner(System.in);
-        System.out.print("Armut (Kg):");
-        armut=manav.nextInt();
-        System.out.print("Elma (Kg):");
-        elma=manav.nextInt();
-        System.out.print("Domates (Kg):");
-        domates=manav.nextInt();
-        System.out.print("Muz (Kg):");
-        muz=manav.nextInt();
-        System.out.print("Patlıcan (Kg):");
-        patlican=manav.nextInt();
+        Scanner hesap = new Scanner(System.in);
 
-        toplamTutar = (armut*armutFiyat)+(elma*elmaFiyat)+(domates*domatesFiyat)+(muz*muzFiyat)+(patlican*patlicanFiyat);
-        System.out.print("Toplam Ödenecek Tutar: " + toplamTutar);
+        System.out.print("1. Sayıyı Girin: ");
+        sayi1=hesap.nextInt();
+        System.out.print("2. Sayıyı Girin: ");
+        sayi2=hesap.nextInt();
+        System.out.println("(Toplama:1, Çıkarma:2, Çarpma:3, Bölme:4) İşlem Seçiniz: ");
+        secim=hesap.nextInt();
+
+        switch (secim){
+            case 1 :
+                islem = sayi1+sayi2;
+                System.out.print("İşem sonucu: "+islem);
+                break;
+            case 2 :
+               islem = sayi1-sayi2;
+                System.out.print("İşem sonucu: "+islem);
+                break;
+            case 3:
+                islem = sayi1*sayi2;
+                System.out.print("İşem sonucu: "+islem);
+                break;
+            case 4:
+                islem = sayi1/sayi2;
+                System.out.print("İşem sonucu: "+islem);
+                break;
+        }
+
     }
 }
