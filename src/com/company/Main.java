78a0ce9a3d5ed8924e67abb0e886a3ea21506346
Kabe KUŞ <kabekus.123@gmail.com>
2021-09-25 19:20:19 +0300

    Pratik - Üslü Sayı Hesaplayan Program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,42 +4,18 @@ import  java.lang.Math;
 public class Main {
 
     public static void main(String[] args) {
-        int sayi,total1=1;
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Sayı Girin :" );
-        sayi= scan.nextInt();
 
-        for(int i=1; i<=sayi; i++){
+        int n,k,total=1;
+        Scanner inp= new Scanner(System.in);
+        System.out.print("Üssü Alınacak Sayıyı Girin: ");
+        n= inp.nextInt();
+        System.out.print("Üs Olacak Sayıyı Girin: ");
+        k= inp.nextInt();
 
-            total1*= i;
+        for( int i=1; i<=k; i++){
+
+            total *= n;
         }
-        System.out.print(sayi+ ". Faktöriyel: "+total1 );
-
-
-        //Ödev
-
-        int a,n,r,totaln=1,totalr=1,total=0, totala=1;
-        Scanner input = new Scanner(System.in);
-        System.out.print("N Girin :" );
-        n= input.nextInt();
-        System.out.print("R Girin :" );
-        r= input.nextInt();
-
-        for( int i=1; i<=n; i++){
-            totaln *=i;
-        }
-        for(int j=1; j<=r; j++){
-            totalr *=j;
-        }
-        a=(n-r);
-
-        for( int k=1; k<=a; k++){
-
-            totala *=k;
-        }
-        total=(totaln / (totalr * totala)) ;
-        System.out.print( n+"in" + r+"'li"+ " Kombinasyonu: "+ total);
+        System.out.print("cevap: "+ total);
     }
-
-
 }
