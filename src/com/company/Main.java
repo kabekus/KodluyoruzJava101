55c1a7038074907f55d60ec574f55e23c6cc85bf
Kabe KUŞ <kabekus.123@gmail.com>
2021-09-28 18:05:26 +0300

    Pratik - Yıldızlar ile Üçgen Yapımı (Elmas)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,28 @@ import  java.lang.Math;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner diamond = new Scanner(System.in);
+        System.out.print("Büyüklük Girin: ");
+        int star = diamond.nextInt();
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("N Sayısını Giriniz :  ");
-        int n = input.nextInt();
-        double result = 0.0;
-        for (double i =1; i<=n; i++){
-            result+=(1/i);
+        for (int i = 1; i <= star; i++) {
+            for (int j = 1; j <= (star - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i)-1 ; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
-        System.out.print("Sonuç: "+result);
+        for (int i = 1; i <= star; i++) {
+            for (int j = 0; j <= i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = (2*star)-3; k >= (2 * i)-1 ; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
     }
 }
