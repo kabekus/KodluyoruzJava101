8e7cde266bf0d5006f20e2903bb46f8e7e6f5ae1
Kabe KUŞ <kabekus.123@gmail.com>
2021-09-29 01:20:39 +0300

    Ödev - Ters Üçgen Yapımı

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,41 +4,18 @@ import java.lang.ArithmeticException;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner diamond = new Scanner(System.in);
+        System.out.print("Üçgen: ");
+        int star = diamond.nextInt();
 
-        int sayi1, sayi2,ebob=1;
-        Scanner islem =new Scanner(System.in);
-
-        System.out.print("Küçük Sayı: ");
-        sayi1=islem.nextInt();
-        System.out.print("Büyük Sayı: ");
-        sayi2=islem.nextInt();
-
-        for (int i=1; i<=sayi1 ; i++){
-            if ((sayi1%i==0) && (sayi2%i==0)){
-                ebob = i;
+        for (int i = 1; i <= star; i++) {
+            for (int j = 0; j <= i-1; j++) {
+                System.out.print(" ");
             }
-        }
-        System.out.println("EBOB(1): "+ebob);
-
-        System.out.println("*********************");
-
-        for (int j=sayi1; j>=1 ; j--){
-            if ((sayi1%j==0) && (sayi2%j==0)){
-                ebob = j;
-                break;
+            for (int k = (2*star)-3; k >= (2 * i)-1 ; k--) {
+                System.out.print("*");
             }
+            System.out.println(" ");
         }
-        System.out.println("EBOB(2): "+ebob);
-
-        System.out.println("EKOK ***********************");
-
-        for (int i=1 ; i<=(sayi1*sayi2); i++){
-            if ((i%sayi1==0) && (i%sayi2==0)){
-                System.out.println(i);
-                break;
-            }
-        }
-        System.out.println("***********************");
-        System.out.println((sayi1*sayi2)/ebob);
     }
 }
