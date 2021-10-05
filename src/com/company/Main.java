395f391dd8bf4ef0339a90aea44eac80b1f4f8b9
Kabe KUŞ <kabekus.123@gmail.com>
2021-10-06 02:11:58 +0300

    Ödev - Desene Göre Metot Oluşturma

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,51 @@
 package com.company;
 import  java.util.Scanner;
 public class Main {
-    static void asalsayi() {
-        int sayi, kontrol=0;
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Sayı Giriniz :");
-        sayi= scan.nextInt();
-        for (int i = 2; i < sayi; i++) {
-            if (sayi % i == 0) {
-                kontrol = 1;
-                break;
+    static int besCikar(int n, boolean dNoktasi, int sayac) {
+
+        /*
+         n = Kullanıcının girmiş olduğu sayı.
+
+         dNoktasi = Sayının 0'a eşit veya küçük olmasının kontrolünü yapan değişken. Girilen sayı sıfırdan büyük
+         olacağından HER ZAMAN "TRUE"
+
+         sayac = 0 veya -(eksi) değerden önce ekrana basılan sayıların sayısı. Sayaç değişkeni kullanıcıdan alınan
+         n değerine bağlı olarak metot içerisinde değişebilir. Ancak başlangıç değeri HER ZAMAN "0" (sıfır) olmalıdır.
+
+
+         Örnek: 7 2 0 2 7 -> 0'dan önce 2 sayı var (7 ve 2).
+         Sayac değişkeni ile n sayısı artırlmaya başlandığında serinin sonsuza gitmesi engellenecek.
+        */
+
+        if (dNoktasi) {
+            if (n > 0) {
+                System.out.print(n + " ");
+                sayac++;
+                return besCikar(n - 5, true, sayac);
+            } else {
+                return besCikar(n, false, sayac);
+            }
+        } else {
+            System.out.print(n + " ");
+            if (sayac > 0) {
+                sayac--;
+                return besCikar(n + 5, false, sayac);
+            } else {
+                return 0;
             }
         }
-        if(kontrol==0) {
-            System.out.println("Sayı Asaldır.");
-        }else{
-            System.out.println("Sayı Asal Değildir!");
-        }
+
     }
+
     public static void main(String[] args) {
-        asalsayi();
+        int n;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("N Sayısı : ");
+        n = input.nextInt();
+
+        System.out.print("Çıktısı : ");
+        besCikar(n, true, 0);
+
     }
 }
