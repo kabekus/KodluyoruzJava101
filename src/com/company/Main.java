00bef94c89d121014d2ce046e11ba5db19ba98e8
Kabe KUŞ <kabekus.123@gmail.com>
2021-10-09 22:31:35 +0300

    Boks Maçı

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,9 @@ package com.company;
 import  java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Employe personel = new Employe("Kabe Kuş",950 ,30 ,2019);
-        personel.printPerson();
-        Employe personel2 = new Employe("Cemile Keklik",2500 ,45 ,2000);
-        personel2.printPerson();
+        Fighter marc = new Fighter("Ahmet" , 15 , 100, 100, 50);
+        Fighter alex = new Fighter("Mehmet" , 10 , 95, 90, 50);
+        Ring r = new Ring(marc,alex , 85 , 100);
+        r.run();
     }
 }
