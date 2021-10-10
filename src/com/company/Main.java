b63389bd59f0b5f89f40baa41525430b9bc77ea0
Kabe KUŞ <kabekus.123@gmail.com>
2021-10-11 02:28:30 +0300

    Pratik - Dizideki Elemanların Ortalamasını Bulan Program(Harmonik)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,14 @@ package com.company;
 import  java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Fighter marc = new Fighter("Ahmet" , 15 , 100, 100, 50);
-        Fighter alex = new Fighter("Mehmet" , 10 , 95, 90, 50);
-        Ring r = new Ring(marc,alex , 85 , 100);
-        r.run();
+        double[] harmonicList = {9,8,7,6,5,4};
+        double harmonicSerieTotal =0.0;
+        double harmonicSerieAvarage;
+        for (double j : harmonicList) {
+            harmonicSerieTotal += (1 / j);
+        }
+        harmonicSerieAvarage = harmonicList.length / harmonicSerieTotal;
+        System.out.println("Harmonik Seri Toplamı: " + harmonicSerieTotal);
+        System.out.println("Harmonik Seri Ortalaması: " + harmonicSerieAvarage);
     }
 }
