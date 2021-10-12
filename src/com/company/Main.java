3857a7a101fa078f2adfc651481327c498328cd8
Kabe KUŞ <kabekus.123@gmail.com>
2021-10-12 22:21:09 +0300

    Pratik - Çok Boyutlu Diziler ile A (B) Harfi Yazdıran Program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,25 @@
 package com.company;
-import  java.util.Scanner;
-import  java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int[] myList={56, 34, 1, 8, 101, -2, -33};
-
-
-        Scanner num = new Scanner(System.in);
-        System.out.print("Sayı Giriniz: ");
-        int number= num.nextInt();
-
-        int min=number ,max=number;
-
-        for (int i : myList) {
-            if(i>number){
-                if(number==max || i<max){
-                    max=i;
-                }
-            }else{
-                if(number==min || i>min){
-                    min=i;
+        String[][] letter = new String[7][4];
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 3 || i== 6) {
+                    letter[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
                 }
             }
         }
 
-        String list = Arrays.toString(myList);
-        System.out.println("Dizi: " + list);
-        System.out.println("Girilen Sayı: " + number);
-        System.out.println("Sayıya yakın küçük sayı: "+min);
-        System.out.println("Sayıya yakın büyük sayı: "+max);
-
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
     }
 }
