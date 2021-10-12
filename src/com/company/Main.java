0b22d59d035cd40f88f24df5388f86c5cfc33059
Kabe KUŞ <kabekus.123@gmail.com>
2021-10-12 22:49:05 +0300

    Pratik - Dizideki Tekrar Eden (çift) Sayıları Bulan Program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,34 @@
 package com.company;
 public class Main {
+    public static boolean isFind(int[] arr,int value){
+        for (int i :arr){
+            if (i==value){
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
+        int[] numList={9,13,51,88,60,13,64,10,10,13,88,60};
+        int[] repeatList=new int[numList.length];
+        int startIndex = 0;
+        for (int i=0 ; i<numList.length ; i++) {
+            for (int j = 0; j < numList.length; j++) {
+                if ((i != j) && (numList[i] == numList[j])) {
+                    if (!isFind(repeatList, numList[i])) {
+                        if (numList[i]%2==0){
+                            repeatList[startIndex++] = numList[i];
+                        }
 
-        String[][] letter = new String[7][4];
-        for (int i = 0; i < letter.length; i++) {
-            for (int j = 0; j < letter[i].length; j++) {
-                if (i == 0 || i == 3 || i== 6) {
-                    letter[i][j] = " * ";
-                } else if (j == 0 || j == 3) {
-                    letter[i][j] = " * ";
-                } else {
-                    letter[i][j] = "   ";
+                    }
+                    break;
                 }
             }
         }
-
-        for (String[] row : letter){
-            for (String col : row){
-                System.out.print(col);
+        for (int value :repeatList){
+            if (value!=0){
+                System.out.print(value+",");
             }
-            System.out.println();
         }
     }
 }
