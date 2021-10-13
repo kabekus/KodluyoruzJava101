dc3f5ef8176f24e2cc0c7768156bcdb335089289
Kabe KUŞ <kabekus.123@gmail.com>
2021-10-13 23:25:00 +0300

    Pratik - Sayı Tahmin Oyunu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,26 +3,55 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int[][] matrix = {
-                { 1, 2, 3 },
-                { 4, 5, 6}
-        };
+        int number = (int) (Math.random() * 100);
 
-        int[][] clone = new int[matrix[0].length][matrix.length];
+        Scanner input = new Scanner(System.in);
+        int right = 0;
+        int selected;
+        int[] wrong = new int[5];
+        boolean isWin = false;
+        boolean isWrong = false;
 
-        System.out.println("Matris : ");
-        for (int i = 0; i < matrix.length; i++) {
-            for (int k = 0; k < matrix[i].length; k++) {
-                clone[k][i] = matrix[i][k];
-                System.out.print(" " + matrix[i][k]);
+        System.out.println(number);
+        while (right < 5) {
+            System.out.print("Lütfen tahmininizi giriniz : ");
+            selected = input.nextInt();
+
+            if (selected < 0 || selected > 99) {
+                System.out.println("Lütfen 0-100 arasında bir değer giriniz.");
+                if (isWrong) {
+                    right++;
+                    System.out.println("Çok fazla hatalı giriş yaptınız. Kalan hak : " + (5 - right));
+                } else {
+                    isWrong = true;
+                    System.out.println("Bir daha hatalı girişinizde hakkınızdan düşülecektir.");
+                }
+                continue;
             }
-            System.out.println();
+
+            if (selected == number) {
+                System.out.println("Tebrikler, doğru tahmin ! Tahmin ettiğini sayı : " + number);
+                isWin = true;
+                break;
+            } else {
+                System.out.println("Hatalı bir sayı girdiniz !");
+                if (selected > number) {
+                    System.out.println(selected + " sayısı, gizli sayıdan büyüktür.");
+                } else {
+                    System.out.println(selected + " sayısı, gizli sayıdan küçüktür.");
+                }
+
+                wrong[right++] = selected;
+                System.out.println("Kalan hakkı : " + (5 - right));
+            }
         }
-        System.out.println("Transpoze : ");
-        for (int i = 0; i < clone.length; i++) {
-            for (int k = 0; k < clone[i].length; k++)
-                System.out.print(" " + clone[i][k]);
-            System.out.println();
+
+        if (!isWin) {
+            System.out.println("Kaybettiniz ! ");
+            if (!isWrong) {
+                System.out.println("Tahminleriniz : " + Arrays.toString(wrong));
+            }
         }
+
     }
 }
