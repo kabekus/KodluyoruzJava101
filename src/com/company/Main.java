8c7b12492a2d645f4f8836181f81ee64f7c1f338
Kabe KUŞ <kabekus.123@gmail.com>
2021-10-13 23:37:40 +0300

    Pratik - Palindromik Kelimleri Bulan Program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,55 +3,31 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int number = (int) (Math.random() * 100);
-
-        Scanner input = new Scanner(System.in);
-        int right = 0;
-        int selected;
-        int[] wrong = new int[5];
-        boolean isWin = false;
-        boolean isWrong = false;
-
-        System.out.println(number);
-        while (right < 5) {
-            System.out.print("Lütfen tahmininizi giriniz : ");
-            selected = input.nextInt();
-
-            if (selected < 0 || selected > 99) {
-                System.out.println("Lütfen 0-100 arasında bir değer giriniz.");
-                if (isWrong) {
-                    right++;
-                    System.out.println("Çok fazla hatalı giriş yaptınız. Kalan hak : " + (5 - right));
-                } else {
-                    isWrong = true;
-                    System.out.println("Bir daha hatalı girişinizde hakkınızdan düşülecektir.");
-                }
-                continue;
-            }
-
-            if (selected == number) {
-                System.out.println("Tebrikler, doğru tahmin ! Tahmin ettiğini sayı : " + number);
-                isWin = true;
-                break;
-            } else {
-                System.out.println("Hatalı bir sayı girdiniz !");
-                if (selected > number) {
-                    System.out.println(selected + " sayısı, gizli sayıdan büyüktür.");
-                } else {
-                    System.out.println(selected + " sayısı, gizli sayıdan küçüktür.");
-                }
-
-                wrong[right++] = selected;
-                System.out.println("Kalan hakkı : " + (5 - right));
-            }
-        }
-
-        if (!isWin) {
-            System.out.println("Kaybettiniz ! ");
-            if (!isWrong) {
-                System.out.println("Tahminleriniz : " + Arrays.toString(wrong));
-            }
-        }
-
+        Scanner inp =new Scanner(System.in);
+        System.out.print("Bir kelime girin: ");
+        String word=inp.nextLine();
+        System.out.println(isPalindrome2(word));
     }
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    static boolean isPalindrome2(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+        if (str.equals(reverse))
+            return true;
+        else
+            return false;
+    }
+
+
 }
